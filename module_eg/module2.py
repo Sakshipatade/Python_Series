07a1fce9e9562a3f1dcea24f138f974aa455af5f
Sakshipatade <sakshivpatade@gmail.com>
2026-05-03 14:06:29 +0530

    folder for modules and packages examples

---
+++ module_eg/module2.py
@@ -0,0 +1,5 @@
+from sakshi_math import NAMES, add, DISTS
+
+print(add(3, 4))
+print(NAMES)
+print(DISTS['ages'])
---
+++ module_eg/sakshi_math.py
@@ -0,0 +1,6 @@
+def add(num1 , num2):
+    result = num1 + num2 
+    return result
+
+NAMES = "akshay"
+DISTS = {"name":"akshay","age":123}
