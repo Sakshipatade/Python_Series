c323cdfdd9ff9c7241d95bc42e9b917ae37877c3
Sakshipatade <sakshivpatade@gmail.com>
2026-05-05 17:25:10 +0530

    check whether the folder exists or not

--- module_eg/osmodule_example.py
+++ module_eg/osmodule_example.py
@@ -1,8 +1,15 @@
 import os 
 # print(os.getcwd())
+
+# to print the number of items in the current directory
 count_of_dir = [i for i in os.listdir()]
-print(len(count_of_dir))
+# print(len(count_of_dir))
 
 
-
+if not os.path.exists('test_folder'):
+    os.mkdir('test_folder')
+    if os.path.isdir('test_folder'):
+        print('folder exists')
+    else:
+        print('folder not found')
 
