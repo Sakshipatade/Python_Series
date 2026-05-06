846f61f400313988afca1fa79ad04f46ecde8e02
Sakshipatade <sakshivpatade@gmail.com>
2026-05-06 17:53:28 +0530

    code to find duplicate items in the list

--- Python_Topics/count_repeated_numbers_in_list.py
+++ Python_Topics/count_repeated_numbers_in_list.py
@@ -1,8 +1,11 @@
-lst = [3, 4, 2, 6, 2, 8, 1, 6, 4, 0, 2, 4, 3, 7, 8]
-seen = []
+lst = [2, 4, 2, 6, 7, 4, 8, 3, 1, 1, 4, 5, 8, 9, 7, 5, 1, 1, 2]
 
+seen = []
+# this will print unique items in list
 for i in lst:
     if i not in seen:
         seen.append(i)
 print(seen)
 
+for i in seen:
+    print(f'Count of {i}: {seen.count(i)}')
