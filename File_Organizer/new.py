99bfc4ffa98223d3846f1f803151e71980c60b5d
Sakshipatade <sakshivpatade@gmail.com>
2026-05-08 13:11:36 +0530

    File Organizer Project

---
+++ File_Organizer/new.py
@@ -0,0 +1,22 @@
+import os 
+import shutil
+PATH = '/home/sakshi/Documents/python-concepts/File_Organizer'
+print(os.getcwd())
+
+file1 = 'file1.txt'
+file2 = 'file2.txt'
+file3 = 'file3.doc'
+file4 = 'file4.pdf'
+file5 = 'file5.pdf'
+
+
+with open(os.path.join(PATH, file1), 'w+')as f1:
+    pass
+with open(os.path.join(PATH, file2), 'w+')as f2:
+    pass
+with open(os.path.join(PATH, file3), 'w+')as f3:
+    pass
+with open(os.path.join(PATH, file4), 'w+')as f4:
+    pass
+with open(os.path.join(PATH, file5), 'w+')as f5:
+    pass
